65abaff2df2304feec458f83f71b54a227b3a18d
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-26 18:21:58 +0530

    Need to understand the process

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,23 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
+    int k,b;
 
     public Line(int k, int b) {
+        this.k=k;
+        this.b=b;
 
     }
-
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        if(this.k== other.k)
+            return null;
+        else{
+            int x=Math.abs((other.b-this.b)/(this.k- other.k));
+            int y= Math.abs(this.k*x+this.b);
+            return new Point(x,y);
+        }
+        //throw new UnsupportedOperationException();
     }
+
 
 }
